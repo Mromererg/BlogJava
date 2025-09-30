fe25f9c1d29df752311e09b5ee770d5daf5e7628
Mromererg <omererginoguz@gmail.com>
2025-09-30 17:40:26 +0300

    ekleme başarılı

---
+++ main.java
@@ -0,0 +1,11 @@
+
+import java.net.SocketTimeoutException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class main {
+    public static void main(String[] args) throws SQLException {
+
+    }
+}
---
+++ sqlConnection.java
@@ -0,0 +1,13 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class sqlConnection {
+    private static final String url = "jdbc:mysql://localhost:3306/blog";
+    private static final String user = "root";
+    private static final String password = "Omer.erg.6565";
+}
+
+public static Connection getConnection() throws SQLException {
+    return DriverManager.getConnection(null, null, null);
+}
